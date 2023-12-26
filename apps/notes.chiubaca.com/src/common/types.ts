dabfd5606429768878021872056a72ec41526176
Alex Chiu <alexchiu11@gmail.com>
2023-12-26 14:33:21 +0000

    WIP:list posts in collection

--- apps/notes.chiubaca.com/src/common/types.ts
+++ apps/notes.chiubaca.com/src/common/types.ts
@@ -3,3 +3,5 @@ import { z } from "astro/zod";
 
 export type NoteTypes = z.infer<typeof noteTypes>;
 export const noteTypes =  z.enum(["fleeting-notes", "index-notes", "literature-notes", "permanent-notes"]);
+
+export type PossiblePaths = NoteTypes | `${NoteTypes}/${string}`
