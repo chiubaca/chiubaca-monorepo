f58491e088c102139d5855efa3feb36c7808d940
Alex Chiu <alexchiu11@gmail.com>
2023-12-26 19:42:53 +0000

    feat: script to update the fleeting note file name to iso-8601 dates

---
+++ tools/md-conversion/update-fleeting-notes-date-file-name.js
@@ -0,0 +1,24 @@
+// loop over all files in tmp/fleeting-notes
+// the file name is a date in the format 20231024.md but it
+// needs to be updated to 2023-10-24.md
+const fs = require("fs");
+const path = require("path");
+
+const fleetingNotesDir = path.join(__dirname, "../../tmp/fleeting-notes");
+
+fs.readdirSync(fleetingNotesDir).forEach((filename) => {
+
+  // only do this action if the file is an md file
+  if (!filename.endsWith(".md")) {
+    return;
+  }
+
+  const date = filename.slice(0, 8);
+  const newDate = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`;
+  const newFilename = `${newDate}.md`;
+
+  fs.renameSync(
+    path.join(fleetingNotesDir, filename),
+    path.join(fleetingNotesDir, newFilename),
+  );
+});
