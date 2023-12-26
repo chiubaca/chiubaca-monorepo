01870b0839d923e1e93db56e513bd33ed0e7ab81
Alex Chiu <alexchiu11@gmail.com>
2023-12-26 19:45:18 +0000

    doc: update script desc

--- tools/md-conversion/remove-layout-frontmatter.js
+++ tools/md-conversion/remove-layout-frontmatter.js
@@ -1,14 +1,15 @@
+// Remove the layout property frontmatter from all markdown files.
+
 const { read, batch } = require("frontmatter-file-batcher");
 
-// A sane limit of 100 concurrent operations.
-batch("tmp/fleeting-notes/**.md", 100, async ({ goods, actions }) => {
+
+batch("tmp/**/**.md", 100, async ({ goods, actions }) => {
  
   const { update, save } = actions;
  
 
   delete goods.data.layout;
- 
-  // // At the end you can save your post with the new data.
+
   await save(goods);
  
   console.log("Just saved:", goods.path);
