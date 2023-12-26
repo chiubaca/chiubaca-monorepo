6ac54b0c5381d4fd3ad313b06b1b0f57f08c2463
Alex Chiu <alexchiu11@gmail.com>
2023-12-26 14:14:05 +0000

    refactor: improving typings

--- apps/notes.chiubaca.com/src/common/types.ts
+++ apps/notes.chiubaca.com/src/common/types.ts
@@ -1,4 +1,5 @@
 
 import { z } from "astro/zod";
 
+export type NoteTypes = z.infer<typeof noteTypes>;
 export const noteTypes =  z.enum(["fleeting-notes", "index-notes", "literature-notes", "permanent-notes"]);
