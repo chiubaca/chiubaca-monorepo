7d9db339f180c55faf75ed96af2c0874d8d8954b
Alex Chiu <alexchiu11@gmail.com>
2023-12-27 17:06:38 +0000

    feat: handle image for notes by moving them to public

--- apps/notes.chiubaca.com/scripts/move-notes.sh
+++ apps/notes.chiubaca.com/scripts/move-notes.sh
@@ -1,3 +1,4 @@
+# move notes to astro content directory
 dirs=(
       "tmp/fleeting-notes"
       "tmp/index-notes"
@@ -9,6 +10,25 @@ for dir in "${dirs[@]}"
 do
   mv "$dir" "apps/notes.chiubaca.com/src/content/"
 done
- 
 
-rm -rf tmp
+# move images to astro public directory
+content_dirs=("apps/notes.chiubaca.com/src/content/fleeting-notes" 
+             "apps/notes.chiubaca.com/src/content/index-notes"
+             "apps/notes.chiubaca.com/src/content/permanent-notes"
+             "apps/notes.chiubaca.com/src/content/literature-notes")
+
+for dir in "${content_dirs[@]}"
+do
+  if [ -d "$dir" ]
+  then
+    for file in "$dir"/*
+    do
+      if [[ $file == *.jpg || $file == *.jpeg || $file == *.png || $file == *.gif ]]
+      then
+        mv "$file" "apps/notes.chiubaca.com/public"
+      fi
+    done
+  fi
+done
+
+# rm -rf tmp
