41a5492ae034b4208f047e4a87ba8a2dc1a8e02d
Alex Chiu <alexchiu11@gmail.com>
2023-12-27 10:44:15 +0000

    fix: error handle when no publish date is available

--- tools/md-conversion/update-frontmatter-publish-date.js
+++ tools/md-conversion/update-frontmatter-publish-date.js
@@ -5,14 +5,27 @@ const { read, batch } = require("frontmatter-file-batcher");
 
 
 batch("tmp/**/**.md", 100, async ({ goods, actions }) => {
-  const { update, save } = actions;
 
-  const date = `${goods.data['publish_date']}`.slice(0, 8);
-  const newDate = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`;
+  try{
+    const { update, save } = actions;
 
-  goods.data["publish_date"] = newDate
+    const date = `${goods.data['publish_date']}`.slice(0, 8);
+    const newDate = `${date.slice(0, 4)}-${date.slice(4, 6)}-${date.slice(6)}`;
+  
+    if(!goods?.data?.["publish_date"]){
+      console.warn("No publish date for:", goods.path);
+      return
+    }
 
-  await save(goods);
- 
-  console.log("Updated publish date for:", goods.path);
+    goods.data["publish_date"] = newDate
+  
+    await save(goods);
+   
+    console.log("Updated publish date for:", goods.path);
+  }catch(err){
+
+    console.warn("Error updating publish date for:", goods.path);
+    console.warn(err);
+  }
+
 });
