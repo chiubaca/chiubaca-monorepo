7b0620986a26a971f34a134afbaa982e6dd5c2a5
Alex Chiu <alexchiu11@gmail.com>
2023-12-28 14:49:26 +0000

    fix: render titles for note collections and dates for fleeting notes

--- apps/notes.chiubaca.com/src/content/config.ts
+++ apps/notes.chiubaca.com/src/content/config.ts
@@ -3,17 +3,25 @@ import { defineCollection } from "astro:content";
 import z from "astro/zod";
 import { noteTypes } from "../common/types";
 
-const noteCollection = defineCollection({
+const fleetingNoteCollection = defineCollection({
   type: "content",
   schema: z.object({
     publish_date: z.string().transform((d) => new Date(d)),
   }),
 });
 
+const noteCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    publish_date: z.string().transform((d) => new Date(d)),
+    title: z.string(),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  [noteTypes.enum["fleeting-notes"]]: noteCollection,
+  [noteTypes.enum["fleeting-notes"]]: fleetingNoteCollection,
   [noteTypes.enum["index-notes"]]: noteCollection,
   [noteTypes.enum["literature-notes"]]: noteCollection,
   [noteTypes.enum["permanent-notes"]]: noteCollection,
