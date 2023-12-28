4cdde09a81abb309f68fb50d82c23a9cdaf0370e
Alex Chiu <alexchiu11@gmail.com>
2023-12-28 13:00:30 +0000

    chore: install and setup daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./libs/chiubaca-ui/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
     "./apps/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./libs/chiubaca-ui/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
