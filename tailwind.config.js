3e9e9cc0a08fddba38f3f1eac89a71495e5a9247
Alex Chiu <alexchiu11@gmail.com>
2023-12-31 11:51:15 +0000

    fix: tweak daisy dark mode settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,7 @@ module.exports = {
   plugins: [require("daisyui"), require("@tailwindcss/typography")],
   daisyui: {
     themes: ["light", "night"],
+    darkTheme: "night",
+    logs: false,
   },
 };
