2ade8913d2a8be9278d306b9c14821f62ef49f87
Alex Chiu <alexander.chiu@houseful.co.uk>
2025-06-14 10:05:25 +0100

    test open graph  with astro integration

--- apps/chiubaca.com/astro.config.ts
+++ apps/chiubaca.com/astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import alpinejs from "@astrojs/alpinejs";
 import tailwindcss from "@tailwindcss/vite";
 import cloudflare from "@astrojs/cloudflare";
+import og from "astro-og";
 
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
@@ -9,7 +10,10 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { updateImageUrls, updateMdLinkUrls } from "../../libs/remark-plugins";
 
 export default defineConfig({
-  integrations: [alpinejs()],
+  integrations: [
+    alpinejs(), // https://alpinejs.dev/
+    og(), // https://github.com/morinokami/astro-og
+  ],
 
   vite: {
     plugins: [tailwindcss()],
